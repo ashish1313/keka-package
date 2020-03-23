119a3eb66d5535e64909279d1b65e36f8fe84c5b
Somnath Jadhav <somnath.jadhav@lapr075.successivetech.com>
2020-03-23 14:00:37 +0530

    updated

--- src/config/keka.php
+++ src/config/keka.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'keka_client_id' => '',
-    'keka_secret_key'=> '',
-    'api_key' => ''
+    'keka_client_id' => env('keka_client_id', ''),
+    'keka_secret_key'=> env('keka_secret_key', ''),
+    'api_key' => env('api_key', '')
 ];
